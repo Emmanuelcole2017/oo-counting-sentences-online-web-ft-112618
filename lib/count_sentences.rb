c5410cfb46508f51616c873e3c97dfc0ff611548
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-12-08 13:37:14 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #binding.pry
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
 
+    if(self.length != 0)
+      self.split('.').collect {|element|
+        binding.pry
+        if element.split('!')
+          element.split('!').each do |element2|
+            self << element2
+          end
+        end
+        }.count
+
+    else
+      0
+    end
   end
 end
